c91f4b906e929fb2c403c0de5b09f6fc12180fa5
Angel Salinas Perez <a.23308051281160@cbtis128.edu.mx>
2026-04-26 18:19:23 -0600

    Add files via upload

--- 8 Ball Magic/script.js
+++ 8 Ball Magic/script.js
@@ -1,4 +1,4 @@
-const answers = ["Sí", "No", "Tal vez", "Probablemente sí", "Probablemente no", "Intentarlo de nuevo"];
+const answers = ["Yes", "No", "Maybe", "Probably yes", "Probably yes", "Try again"];
 const askBtn = document.getElementById("askBtn");
 const answerText = document.getElementById("answerText");
 const questionInput = document.getElementById("question");
@@ -8,22 +8,21 @@ const emoji = document.querySelector(".emo");
 askBtn.addEventListener("click", () => {
   const question = questionInput.value.trim();
   if(question === "") {
-    alert("Escribe una pregunta cerrada.");
+    alert("Write closed question");
     return;
   }
 
-  // Elegir respuesta aleatoria
+
   const randomIndex = Math.floor(Math.random() * answers.length);
   answerText.textContent = answers[randomIndex];
 
-  // Ocultar el emoji
+
   emoji.style.display = "none";
 
-  // Borrar contenido del input
+
   questionInput.value = "";
 
-  // Reiniciar animación: quitar clase y forzar reflow
   ball.classList.remove("show");
-  void ball.offsetWidth; // fuerza que el navegador registre el cambio
+  void ball.offsetWidth; 
   ball.classList.add("show");
 });
